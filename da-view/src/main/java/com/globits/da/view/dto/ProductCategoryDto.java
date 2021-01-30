f8d5e76ffd8d2acdfe1ac9e77f4e2f143af4e180
kien1501 <mchv4400>
2021-01-30 12:10:11 +0700

    2

--- da-view/src/main/java/com/globits/da/view/dto/ProductCategoryDto.java
+++ da-view/src/main/java/com/globits/da/view/dto/ProductCategoryDto.java
@@ -1,8 +1,11 @@
 package com.globits.da.view.dto;
 
+import java.util.List;
+
 public class ProductCategoryDto {
 	private String name;
 	private String code;
+	private List<ProductDto> listProduct;
 	public String getName() {
 		return name;
 	}
@@ -14,6 +17,12 @@ public class ProductCategoryDto {
 	}
 	public void setCode(String code) {
 		this.code = code;
+	}
+	public List<ProductDto> getListProduct() {
+		return listProduct;
+	}
+	public void setListProduct(List<ProductDto> listProduct) {
+		this.listProduct = listProduct;
 	}
 	
 	
